8145a0d6a972ffb843412a96c0b2acfac9cd3b16
bianxindong <bxd_best@163.com>
2022-09-08 15:22:18 +0800

    update

--- data_converter.py
+++ data_converter.py
@@ -1,3 +1,4 @@
+from ast import arg
 from email.mime import image
 import json
 import os
@@ -33,6 +34,14 @@ def subfiles(folder: str, join: bool = True, prefix: str = None, suffix: str = N
         res.sort()
     return res
 
+def copy_directory(source: str,target: str):
+    if os.path.exists(target):
+        shutil.rmtree(target)
+
+    ignore_files=list(["processing","preprocessing","models","raw","reviewed"])
+    if os.path.exists(source):
+        shutil.copytree(source, target,ignore=shutil.ignore_patterns("processing","preprocessing","models","raw","reviewed"))
+        print('copy dir finished!')
 
 def nifti_files(folder: str, join: bool = True, sort: bool = True) -> List[str]:
     return subfiles(folder, join=join, sort=sort, suffix='.nii.gz')
@@ -59,8 +68,9 @@ def save_volume_to_task_directory(cases:list,orgin_dir:str,task_dir:str,test_cou
     test_list = []
     i = 0
     for key in keys:
-        label = cases[key]
-        img = label[:-13] + ".nii.gz"
+        label = cases[key][:-7] + ".nii.gz"
+        img = cases[key][:-13] + ".nii.gz"
+        targetlabel = cases[key][:-18] + ".nii.gz"
 
         print(f"label:{label},img:{img}")
 
@@ -70,23 +80,48 @@ def save_volume_to_task_directory(cases:list,orgin_dir:str,task_dir:str,test_cou
             shutil.move(os.path.join(orgin_dir, img),
                         os.path.join(task_dir, "imagesTs", img))
             shutil.move(os.path.join(orgin_dir, label),
-                        os.path.join(task_dir, "labelsTr", label))                        
+                        os.path.join(task_dir, "labelsTr", targetlabel))                        
         else:
             train_list.append(img)
 
             shutil.move(os.path.join(orgin_dir, img),
                         os.path.join(task_dir, "imagesTr", img))
             shutil.move(os.path.join(orgin_dir, label),
-                        os.path.join(task_dir, "labelsTr", label))
+                        os.path.join(task_dir, "labelsTr", targetlabel))
         i = i+1
     
     return (train_list,test_list)
 
+def rename_files(file_path:str):
+    work_dir = get_parent_dir(file_path)
+    cases = subfiles(file_path, join=False,suffix= ".nii.gz")
+    root_dir = os.path.join(work_dir, "labels_new")
+    os.makedirs(root_dir, exist_ok=True)
+    for it in cases:
+        print(f"{it}\n")
+        shutil.copy(os.path.join(file_path, it),
+        os.path.join(root_dir, it[:-7] + "_0000_label.nii.gz"))
 
-def convert(input: str, task_name: str,test_num:int = -1) -> None:
+def rename_nii_file(file_path:str):
+
+    work_dir = get_parent_dir(file_path)
+    cases = subfiles(file_path, join=False,suffix= ".nii.gz")
+    #root_dir = os.path.join(work_dir, "labels_new")
+    #os.makedirs(root_dir, exist_ok=True)
+    for it in cases:
+        print(f"{it}\n")
+        shutil.move(os.path.join(file_path, it),
+        os.path.join(file_path, it[:-7] + "_0000.nii.gz"))
+
+
+
+def convert(input: str, output: str,task_name: str,test_num:int = -1) -> None:
 
     #1.创建任务文件夹
-    work_dir = os.getenv('nnUNet_raw_data_base') +"/nnUNet_raw_data"
+    if len(output) == 0 :
+        work_dir = os.getenv('nnUNet_raw_data_base') +"/nnUNet_raw_data"
+    else:
+        work_dir = output
     #work_dir = get_parent_dir(input)
     root_dir = os.path.join(work_dir, task_name)
     os.makedirs(os.path.join(root_dir, "imagesTr"), exist_ok=True)
@@ -94,8 +129,8 @@ def convert(input: str, task_name: str,test_num:int = -1) -> None:
     os.makedirs(os.path.join(root_dir, "labelsTr"), exist_ok=True)
     print(root_dir)
 
-    #2.读取lable获取，获取标注总数
-    cases = subfiles(input, join=False,suffix= "label.nii.gz")
+    #2.读取lable获取，获取label总数
+    cases = subfiles(os.path.join(input,"marked"), join=False,suffix= "label.nii.gz")
     print(cases)
     n = len(cases)
    
@@ -114,41 +149,73 @@ def convert(input: str, task_name: str,test_num:int = -1) -> None:
 
     #3.随机分配数据去test和train
     #dataset_file_list = save_volume_to_task_directory(input,root_dir,test_num=2)
-    dataset_train_files,dataset_test_files = save_volume_to_task_directory(cases,input,root_dir,test_count,n)
+    dataset_train_files,dataset_test_files = save_volume_to_task_directory(cases,os.path.join(input,"marked"),root_dir,test_count,n)
 
     # save json
-    with open("C:/Users/Bxd/Desktop/data_test/labels.json", 'r') as f:
-        temp = json.loads(f.read())
-        #print(temp)
-        print(temp['labels'])
-
     json_dict = OrderedDict()
-    json_dict['name'] = task_name
-    json_dict['description'] = "Feature points "
-    json_dict['tensorImageSize'] = "3D"
-    json_dict['reference'] = "see visual3d database"
-    json_dict['licence'] = "see visual3d database"
-    json_dict['release'] = "0.0"
-    json_dict['modality'] = {
-        "0": "CT",
-    }
-    json_dict['labels'] = temp['labels']
+
+    
+    with open(os.path.join(input,"task.json"), 'r',encoding='UTF-8') as f:
+        task_json = json.loads(f.read())
+
+        json_dict['name'] = task_name
+        json_dict['description'] = task_json['description']
+        json_dict['tensorImageSize'] =  task_json['tensorImageSize']
+        json_dict['reference'] =  task_json['reference'] 
+        json_dict['licence'] = task_json['licence']
+        json_dict['relase'] = task_json['relase']
+        json_dict['modality'] =  task_json['modality']
+  
+  
+    with open(os.path.join(input,"labels.json"), 'r',encoding='UTF-8') as f:
+        labels_json = json.loads(f.read())
+        json_dict['labels'] = labels_json['labels']
+        #print(temp)
+        print(labels_json['labels'])
+
     json_dict['numTraining'] = train_count
     json_dict['numTest'] = test_count
 
-    json_dict['training'] = [{'image': "imagesTr/%s" % i, "label": "labelsTr/%s.label.nii.gz" % i[:-7]} for i in
+    json_dict['training'] = [{'image': "./imagesTr/%s.nii.gz" % i[:-12], "label": "./labelsTr/%s.nii.gz" % i[:-12]} for i in
                             dataset_train_files]
     json_dict['validation'] =  []
-    json_dict['test'] = ["imagesTs/%s" %
-                        i for i in dataset_test_files]
+    json_dict['test'] = ["./imagesTs/%s.nii.gz" %
+                        i[:-12] for i in dataset_test_files]
 
     save_json(json_dict, os.path.join(root_dir, "dataset.json"))
 
 if __name__ == '__main__':
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument("-i", '--input_dir', help= "source directory", required=False)
-    #parser.add_argument('-t', "--Task_xxxx", required=False, help="task name")
-    #parser.add_argument('-m', '--mapping_file', help='id mapping file.',required=False)
-    convert("C:/Users/Bxd/Desktop/data_test", "Task188_test",1)
-    #work_dir = os.getenv('nnUNet_raw_data_base')
+
+    default_task_nanme = 'Task201_pelvic'
+    default_input = "C:/Users/Bxd/Desktop/task_pelvis"
+    default_temp_dir = os.path.join(os.getenv('nnUNet_raw_data_base'),"temp",os.path.basename(default_input))
+    default_output = os.getenv('nnUNet_raw_data_base') +"/nnUNet_raw_data"
+    default_num = -1
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", '--input',default=default_input, help= "source directory", required=False)
+    parser.add_argument('-t', "--task_name",default=default_task_nanme, required=False, help="taskNUM_XXX")
+    parser.add_argument('-tmp', '--temp', default=default_temp_dir,help='temporary folders',required=False)
+    parser.add_argument('-o', '--output', default=default_output,help='output directory',required=False)
+    parser.add_argument('-n', '--test_number', default=default_num,help='test set number',required=False)
+
+    args = parser.parse_args()
+    print(f"input_dir:{args.input}\n")
+    print(f"temp_dir:{args.temp}\n")
+    print(f"output_dir:{args.output}\n")
+    print(f"task_name:{args.task_name}\n")
+
+    copy_directory(args.input,args.temp)
+    convert(args.temp,args.output,args.task_name,args.test_number)
+    shutil.rmtree(args.temp)
+    # convert("C:/Users/Bxd/Desktop/data_test", "Task188_test",1)
+    # work_dir = os.getenv('nnUNet_raw_data_base')
     #print(work_dir)
+
+    #input_dir = "//192.168.100.4/label/markroot/task_pelvis"
+   
+
+    #rename_files("\\\\192.168.100.4\\Data\\meddata\\KiTS2021\\labelsTr")
+    #rename_nii_file("\\\\192.168.100.4\\label\\markroot\\task_pelvis\\raw")
+ 
--- test1.py
+++ test1.py
@@ -16,3 +16,119 @@ x = torch.randn(1,100,98)
 n = torch.stack([w,x])
 # print(y.shape)
 print(n.shape)
+
+#%%
+import os
+model_file = '/home/bian/project/data/nnUNet_trained_models/nnUNet/3d_lowres/Task135_KiTS2021/nnUNetTrainerV2__nnUNetPlansv2.1/fold_0/model_final_checkpoint.model'  
+folder, f  = os.path.split(model_file)
+print(f)
+# %%
+import torch
+import torch.autograd.variable as Variable
+t1 = torch.tensor([1])
+t1 = t1.cuda()
+print(t1)
+a=Variable(torch.Tensor([1]),requires_grad=True).cuda()
+a
+# %%
+ret = {'name': "bian"}
+ret['input'] = torch.tensor([0.0,1.0,0.8])
+ret['gt'] = torch.tensor([1.1,1.2,1.3])
+ret[0]
+# %%
+import shutil
+import os
+import time
+os.makedirs("./myTestidr",exist_ok=True)
+time. sleep(5)
+#shutil.rmtree("./myTestidr")
+os.removedirs("./myTestidr")
+# %%
+
+def Add(a,b):
+    return a+b
+def Sub(a,b):
+    return a-b
+
+
+method={
+    "add":Add,
+    "sub":Sub
+}
+
+
+print(method["add"](1,2))
+# %%
+
+
+class A:
+
+    def __init__(self) -> None:
+        
+        self.num = self.get_num()
+        print(self.num)
+
+    def get_num(self)->int:
+        return 5
+
+class B(A):
+    def __init__(self) -> None:
+        super().__init__()
+    def get_num(self)->int:
+        return 10
+
+btest = B()
+# %%
+a = True
+
+print(type(a))
+
+# %%
+import torch
+import torch.nn as nn
+conv1 = nn.Conv1d(in_channels=256, out_channels=100, kernel_size=2)
+input = torch.randn(32, 35, 256)
+input = input.permute(0, 2, 1)
+output = conv1(input)
+print(output.shape)
+# %%
+class CNN(nn.Module):
+    def __init__(self):
+        nn.Model.__init__(self)
+ 
+        self.conv1 = nn.Conv2d(1, 6, 5)  # 输入通道数为1，输出通道数为6
+        self.conv2 = nn.Conv2d(6, 16, 5)  # 输入通道数为6，输出通道数为16
+        self.fc1 = nn.Linear(5 * 5 * 16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # 输入x -> conv1 -> relu -> 2x2窗口的最大池化
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        # 输入x -> conv2 -> relu -> 2x2窗口的最大池化
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        # view函数将张量x变形成一维向量形式，总特征数不变，为全连接层做准备
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+#%%
+
+import time
+# 获取当前时间
+current_time = int(time.time())
+print(current_time) # 1631186249
+# 转换为localtime
+localtime = time.localtime(current_time)
+# 利用strftime()函数重新格式化时间
+dt = time.strftime('%Y-%m-%d-%H-%M-%S', localtime)
+print(dt) # 返回当前时间：2021:09:09 19:17:29
+
+# %%
+import shutil
+os.makedirs()
